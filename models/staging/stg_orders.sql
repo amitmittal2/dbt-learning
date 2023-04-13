b3605d4b1d2f1d3f1001f6d57cba9a286fff4bb6
apmittal@yahoo.com <apmittal@yahoo.com>
2023-04-13 16:56:02 +0000

    Added packages and deliver team seed

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,4 +1,5 @@
 SELECT
+{{ dbt_utils.generate_surrogate_key(['o.orderid', 'c.customerid','p.productid']) }} AS sk_orders,
 o.orderid,
 o.orderdate,
 o.shipdate,
@@ -14,10 +15,13 @@ p.productid,
 p.category,
 p.productname,
 p.subcategory,
-{{ markup('o.ordersellingprice', 'o.ordercostprice' ) }} AS markup_macro
+{{ markup('o.ordersellingprice', 'o.ordercostprice' ) }} AS markup_macro,
+d.delivery_team
 FROM {{ ref('raw_orders') }} o
 LEFT JOIN {{ ref('raw_customers') }} AS c
     ON o.customerid = c.customerid
 LEFT JOIN {{ ref('raw_products') }} AS p
     ON o.productid = p.productid
+LEFT JOIN {{ ref('delivery_team') }} AS d
+    ON o.shipmode = d.shipmode
 {{ macro_limit_data_in_dev('o.orderdate') }}
