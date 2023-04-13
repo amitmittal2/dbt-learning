d1ca05a370384e21523dd7759ddd1ad31e7e0e6a
apmittal@yahoo.com <apmittal@yahoo.com>
2023-04-13 09:58:48 +0000

    developing stg model

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,20 @@
+SELECT 
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode, 
+o.ordersellingprice,
+o.ordercostprice,
+o.ordersellingprice - o.ordercostprice AS orderprofit,
+c.customername,
+c.segment,
+c.country,
+p.category,
+p.productname,
+p.subcategory
+FROM {{ ref('raw_orders') }} o
+LEFT JOIN {{ ref('raw_customers') }} AS c
+    ON o.customerid = c.customerid
+LEFT JOIN {{ ref('raw_products') }} AS p
+    ON o.productid = p.productid
+
